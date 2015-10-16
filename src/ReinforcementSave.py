f7b2f226341f036958368d1e17dc4dd49834dd37
TorbenHaug <claustorbenhaug@hotmail.de>
2015-10-16 18:11:54 +0200

    Changed Saving to values only now it's possible to save a 2**16 value as rating The set and getRatingForNextState respond to wentdirection
    At the moment everything will be loaded from HDD everytime, next step is to implement a
    Site Replace Algorythm

--- src/ReinforcementSave.py
+++ src/ReinforcementSave.py
@@ -6,27 +6,23 @@ import struct
 
 class ReinforcementSave(object):
 	"""docstring for ClassName"""
-	def __init__(self, fileName, numGhosts, maxMemoryInMB, maxBitForRating):
+	def __init__(self, fileName, numGhosts, maxMemoryInMB):
 		self.fileName = fileName
 		self.numGhosts = numGhosts
 		self.maxMemoryInMB = maxMemoryInMB
-		self.maxBitForRating = maxBitForRating
-		self.checkMaxBits()
 		self.initFile()
-
-	def checkMaxBits(self):
-		if ((self.numGhosts * 5) + 2) + self.maxBitForRating > 32:
-			raise ValueError('You are using more than 32 Bit')
 
 	def maxNumberOfStates(self):
 		return 2 ** ((self.numGhosts * 5) + 2)
 
 	def fileSizeInBit(self):
-		return self.maxNumberOfStates() * 32
+		return self.maxNumberOfStates() * self.numGhosts * 16
+
 	def createInitialFile(self, filePath):
 		wf = open(str(filePath), "wb")
 		for x in range(0, self.maxNumberOfStates()):
-			wf.write(struct.pack("I", x << self.maxBitForRating))
+			for y in xrange(0,self.numGhosts):
+				wf.write(struct.pack("H", 0))
 		wf.close()
 
 	def initFile(self):
@@ -42,33 +38,31 @@ class ReinforcementSave(object):
 		wf = open(str(self.filePath), "rb")
 		binVal = state.toBin()
 		print 'BinVal' + str(bin(binVal))
-		wf.seek(binVal*4)
-		tmp = struct.unpack("I", wf.read(4))[0]
+		wf.seek((binVal * 2) + ReinforcementDirection.fromGameDirection(wentDirection))
+		tmp = struct.unpack("H", wf.read(2))[0]
 		print 'tmp' + str(bin(tmp))
 		wf.close()
-		return (binVal << self.maxBitForRating)^tmp
+		return tmp
 
 	def setRatingForState(self, wentDirection, state, rating):
-		if rating > 2**self.maxBitForRating:
+		if rating > 2**16:
 			raise ValueError('Rating to high')
 		wf = open(str(self.filePath), "r+b")
 		binVal = state.toBin()
-		wf.seek(binVal*4)
-		wf.write(struct.pack("I", (binVal << self.maxBitForRating) + rating))
+		wf.seek((binVal * 2) + ReinforcementDirection.fromGameDirection(wentDirection))
+		wf.write(struct.pack("H", rating))
 		wf.close()
 
 
-a = ReinforcementSave('1ghost', 1, 128, 5)
-print str(a.maxNumberOfStates()) + ' ' + str(float(a.fileSizeInBit()) / (8 * 1024 * 1024))
-state = ReinforcementState(Directions.WEST, [GhostState(Directions.NORTH, Threat.DANGER, True)])
-print 'getRatingForNextState' + str(a.getRatingForNextState(Directions.WEST, state))
-a.setRatingForState(Directions.WEST,state,7)
-print 'setRatingForNextState' + str(a.getRatingForNextState(Directions.WEST, state))
-a = ReinforcementSave('2ghost', 2, 128, 5)
-print str(a.maxNumberOfStates()) + ' ' + str(float(a.fileSizeInBit()) / (8 * 1024 * 1024))
-a = ReinforcementSave('3ghost', 3, 128, 5)
-print str(a.maxNumberOfStates()) + ' ' + str(float(a.fileSizeInBit()) / (8 * 1024 * 1024))
-a = ReinforcementSave('4ghost', 4, 128, 5)
-print str(a.maxNumberOfStates()) + ' ' + str(float(a.fileSizeInBit()) / (8 * 1024 * 1024))
-
-print bin(2**16 - 1)
+#a = ReinforcementSave('1ghost', 1, 128)
+#print str(a.maxNumberOfStates()) + ' ' + str(float(a.fileSizeInBit()) / (8 * 1024 * 1024))
+#state = ReinforcementState(Directions.WEST, [GhostState(Directions.NORTH, Threat.DANGER, True)])
+#print 'getRatingForNextState' + str(a.getRatingForNextState(Directions.WEST, state))
+#a.setRatingForState(Directions.WEST,state,7)
+#print 'setRatingForNextState' + str(a.getRatingForNextState(Directions.WEST, state))
+#a = ReinforcementSave('2ghost', 2, 128)
+#print str(a.maxNumberOfStates()) + ' ' + str(float(a.fileSizeInBit()) / (8 * 1024 * 1024))
+#a = ReinforcementSave('3ghost', 3, 128)
+#print str(a.maxNumberOfStates()) + ' ' + str(float(a.fileSizeInBit()) / (8 * 1024 * 1024))
+#a = ReinforcementSave('4ghost', 4, 128)
+#print str(a.maxNumberOfStates()) + ' ' + str(float(a.fileSizeInBit()) / (8 * 1024 * 1024))
